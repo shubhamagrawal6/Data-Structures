99a9aa3ac7e65840c84f5a3ecd93dfb599c550c6
shubham <shubhamagrawal322@gmail.com>
2020-01-03 23:57:52 +0530

    created function but has some error

--- Linked lists/Circular/2.deletenode.cpp
+++ Linked lists/Circular/2.deletenode.cpp
@@ -28,3 +28,94 @@ void printlist(Node *last) {
     }while(temp != last->next);
 
 }
+
+Node* insertempty(Node *last, int data) {
+    if(last != NULL) {
+        return last;
+    }
+
+    Node *temp = new Node(data);
+    
+    last = temp;
+    last->next = last; //Self loop
+
+    return last;
+}
+
+Node* insertbegin(Node* last, int data) {
+    if(last == NULL) {
+        return insertempty(last, data);
+    }
+
+    Node* temp = new Node(data);
+    
+    temp->next = last->next;
+    last->next = temp;
+    
+    return last;
+}
+
+void deletenode(Node **headref, int item) {
+    //If list is empty
+    if(*headref == NULL){
+        cout<<"Empty List \n";
+        return;
+    }
+    
+    //If node to be deleted is at head
+    if((*headref)->data == item){
+        //If list is a self loop
+        if((*headref)->next == (*headref)){
+            delete *headref;
+            *headref = NULL;
+        }
+
+        else{
+            Node *temp  = (*headref)->next;
+            while(temp->next != *headref){
+                temp = temp->next;
+            }
+            temp->next = (*headref)->next;
+            *headref = temp->next;
+        }
+    }
+    
+    //If node to be deleted is anywhere except head
+    Node *temp  = *headref;
+    while(temp != *headref){
+        if(temp->next->data == item){
+            temp->next = temp->next->next;
+            break;
+        }
+        else{
+            temp = temp->next;
+        }
+    }
+}
+
+int main(){
+    Node *itr = new Node(23);
+    printlist(itr);
+    cout<<"test\n";
+    
+    deletenode(&itr,23);
+    printlist(itr);
+    cout<<"test\n";
+
+    itr = insertbegin(itr, 4);
+    itr = insertbegin(itr, 12);
+    itr = insertbegin(itr, 8);
+    itr = insertbegin(itr, 1);
+    itr = insertbegin(itr, 9);
+    itr = insertbegin(itr, 37);
+    printlist(itr);
+    cout<<"test\n";
+
+    deletenode(&itr, 12);
+    printlist(itr);
+    cout<<"test\n";
+    
+    deletenode(&itr, 37);
+    printlist(itr);
+    cout<<"test\n";
+}

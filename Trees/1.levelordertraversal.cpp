9f25ec57e3aa04bf8449e2ca05fcc1d75c5342f6
shubham <shubhamagrawal322@gmail.com>
2020-01-05 15:14:56 +0530

    level order traversal added

---
+++ Trees/1.levelordertraversal.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left, *right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+};
+
+void levelorder(Node *root) {
+    if(root == NULL) {
+        cout<<"Tree is empty\n";
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()) {
+        Node* temp = q.front();
+        cout<<temp->data<<endl;
+        q.pop();
+
+        if(temp->left != NULL) {
+            q.push(temp->left);
+        }
+        if(temp->right != NULL) {
+            q.push(temp->right);
+        }
+    }
+}
+
+int main() { 
+    Node *root = new Node(1); 
+    
+    root->left = new Node(2); 
+    root->right = new Node(3); 
+    root->left->left = new Node(4); 
+    root->left->right = new Node(5); 
+  
+    cout << "Level order traversal is: \n"; 
+    levelorder(root); 
+    
+    return 0; 
+}

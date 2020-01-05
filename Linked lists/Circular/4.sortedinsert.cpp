4463b162b5bb392a2ecfae633a2d71268fe7c4a8
shubham <shubhamagrawal322@gmail.com>
2020-01-05 15:14:18 +0530

    added sorted insert program

---
+++ Linked lists/Circular/4.sortedinsert.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void printlist(Node *head) {
+    
+    if(head == NULL){
+        cout<<"Empty List \n";
+        return;
+    }
+
+    Node *temp = head; 
+    //Since it is circular linked list the next element will only be null if list is empty
+
+    do {
+        cout<<temp->data<<endl;
+        temp = temp->next;
+    }while(temp != head);
+
+}
+
+void sortedinsert(Node **headref, Node *newnode){
+    Node* temp = *headref;
+
+    if(*headref == NULL) {
+        *headref = newnode;
+        (*headref)->next = *headref;
+    }
+    
+    else if((*headref)->data >= newnode->data) {
+        while(temp->next != *headref){
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->next = *headref;
+        *headref = newnode;
+    }
+
+    else {
+        temp = *headref;
+        while(temp->next != *headref && temp->next->data < newnode->data) {
+            temp = temp->next;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+}
+
+int main() {
+    int arr[] = {19, 34, 2, 21, 0, 79};
+    Node *head = NULL, *newnode;  
+
+    // Created linked list will be 0->2->19->21->34->79
+    for (int i = 0; i< 6; i++)  {  
+        newnode = new Node(arr[i]); 
+        sortedinsert(&head, newnode);  
+    }  
+    
+    printlist(head);  
+      
+    return 0;
+}

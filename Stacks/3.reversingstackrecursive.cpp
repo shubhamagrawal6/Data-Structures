8cc6c27ef7207ffb49ea44b6906eba451186d0fb
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-03-15 22:53:43 +0530

    added stack reversing using recursion

---
+++ Stacks/3.reversingstackrecursive.cpp
@@ -0,0 +1,60 @@
+//Reversing a stack recursive method
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Auxiliary Function to print a stack
+void printStack (stack<int> s) {
+    if (s.empty())
+        return;
+    int x = s.top();
+    s.pop();
+    printStack(s);
+    cout << x << " ";
+    s.push(x);
+}
+
+// Utility function for our main logic
+/* NOTE: Here passing stack by reference is important.
+   Work-out with sample problem to understand why. */
+void insertAtBottom (stack<int> &s, int element) {
+    if (s.empty()) {
+        s.push(element);
+        return;
+    }
+    int x = s.top();
+    s.pop();
+    insertAtBottom(s, element);
+    s.push(x);
+}
+
+// Main Logic of Code
+void reverse (stack<int> &s) {
+    if (s.empty())  return;
+    int temp = s.top();
+    s.pop();
+    reverse(s);
+    insertAtBottom(s, temp);
+}
+
+
+int main() {
+    stack<int> st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+
+    cout << "Original Stack:\n";
+    printStack(st);
+    cout << endl;
+
+    reverse(st);
+
+    cout << "Reversed Stack:\n";
+    printStack(st);
+    cout << endl;
+
+    return 0;
+}

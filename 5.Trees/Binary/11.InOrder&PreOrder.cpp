d2295dfd2c9858969ebe9a56d938cf58090aad03
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-10-30 15:02:05 +0530

    deleting node and constructing tree

--- 5.Trees/Binary/11.InOrder&PreOrder.cpp
+++ 5.Trees/Binary/11.InOrder&PreOrder.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 class Node{
     public:
-        int data;
-        Node *left, *right;
-        Node(int data){
-            this->data = data;
-            this->left = this->right = NULL;
-        }
+    int data;
+    Node *left, *right;
+    
+    Node(int data){
+        this->data = data;
+        this->left = this->right = NULL;
+    }
 };
 
 void inorder(Node* root){
